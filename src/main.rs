1c77ed37686de7996ab45559e3305ff15a234255
Bolo101 <iglesias.guillaumedu64@gmail.com>
2026-03-29 11:59:31 +0200

    Added transport mode TCP and UDP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+enum TransportMode {
+    Tcp,             // mode TCP classique
+    Udp { ttl: u8 }, // mode UDP avec un TTL (u8 = entier entre 0 et 255)
+}
+
+fn describe_mode(mode: &TransportMode) {
+    match mode {
+        TransportMode::Tcp => {
+            println!("Mode : TCP (handshake DICOM complet)");
+        }
+        TransportMode::Udp { ttl } => {
+            println!("Mode : UDP brut avec TTL = {}", ttl);
+        }
+    }
+}
+
 struct DicomConfig {
     host: String, // IP address or hostanem
     port: u16,    // network port (u16 = integer between 0 and 65535)
@@ -25,4 +41,10 @@ fn main() {
     println!("Port   : {}", config.port);
     println!("AET    : {}", config.aet);
     config.display();
+
+    let mode_tcp = TransportMode::Tcp;
+    let mode_udp = TransportMode::Udp { ttl: 10 };
+
+    describe_mode(&mode_tcp);
+    describe_mode(&mode_udp);
 }
